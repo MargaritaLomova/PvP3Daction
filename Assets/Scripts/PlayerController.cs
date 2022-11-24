ea02227bc26c854e030e3a978431a28479ac8fe3
Margarita <margaritalomova666@gmail.com>
2022-11-24 15:20:45 +0600

    Adding Mirror asset, bugs fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,16 @@
+using Mirror;
+using System.Collections;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     [Header("Components")]
-    [SerializeField]
-    private CharacterController _characterController;
-    [SerializeField]
-    private Transform _mainCamera;
     [SerializeField]
     private Animator _animator;
     [SerializeField]
     private Transform _model;
+    [SerializeField]
+    private SkinnedMeshRenderer _meshRenderer;
 
     [Space]
     [Header("Variables")]
@@ -18,36 +18,71 @@ public class PlayerController : MonoBehaviour
     private float _speed = 3f;
     [SerializeField]
     private float _rotateSpeed = 5f;
+    [SerializeField]
+    private float _spurtDistance = 3f;
+    [SerializeField]
+    private float _hittedTime = 3f;
+
+    [Space]
+    [Header("Colors")]
+    [SerializeField]
+    private Color _idleColor;
+    [SerializeField]
+    private Color _hittedColor;
+
+    public bool IsSpunt { get; private set; }
+    public bool IsHitted { get; private set; }
 
     private float _targetAngle;
-    private Vector3 _direction;
-
     private float _horizontal;
     private float _vertical;
 
+    private int _countOfHittedPlayers = 0;
+
+    private Vector3 _direction;
+
+    private Transform _mainCamera;
+
     private void Start()
     {
+        _mainCamera = Camera.main.transform;
+
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            GetHit();
+        }
     }
 
     private void FixedUpdate()
     {
-        GetDirection();
+        HandleMovement();
+    }
 
-        _animator.SetBool("RunBool", _direction.magnitude >= 0.1f);
-
-        if (_direction.magnitude >= 0.1f)
+    private void HandleMovement()
+    {
+        if(isLocalPlayer)
         {
-            _targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+            GetDirection();
 
-            Move();
-            Rotate();
-        }
+            _animator.SetBool("RunBool", _direction.magnitude >= 0.1f);
 
-        if(Input.GetMouseButton(0))
-        {
-            _animator.SetBool("RunBool", true);
-            _characterController.Move(_model.forward * _speed * Time.fixedDeltaTime * 15f);
+            if (_direction.magnitude >= 0.1f)
+            {
+                _targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+
+                Move();
+                Rotate();
+            }
+
+            if (Input.GetMouseButtonUp(0) && !IsSpunt)
+            {
+                StartCoroutine(Spunt());
+            }
         }
     }
 
@@ -62,12 +97,60 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         Vector3 moveDirection = Quaternion.Euler(0f, _targetAngle, 0f) * Vector3.forward;
-        _characterController.Move(moveDirection.normalized * _speed * Time.fixedDeltaTime);
+        transform.Translate(moveDirection.normalized * _speed * Time.fixedDeltaTime, Space.World);
     }
 
     private void Rotate()
     {
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_direction), Time.fixedDeltaTime * _rotateSpeed);
         _model.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, Time.fixedDeltaTime * _rotateSpeed);
+    }
+
+    private void GetHit()
+    {
+        if (!IsHitted)
+        {
+            StartCoroutine(ChangeColorToHitted());
+        }
+    }
+
+    private IEnumerator ChangeColorToHitted()
+    {
+        IsHitted = true;
+
+        foreach (var material in _meshRenderer.materials)
+        {
+            material.color = _hittedColor;
+        }
+
+        float passedTime = 0f;
+        while (passedTime < _hittedTime)
+        {
+            passedTime += 0.5f;
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        foreach (var material in _meshRenderer.materials)
+        {
+            material.color = _idleColor;
+        }
+
+        IsHitted = false;
+    }
+
+    private IEnumerator Spunt()
+    {
+        IsSpunt = true;
+        float passedTime = 0f;
+        while (passedTime < 0.5f)
+        {
+            _animator.SetBool("RunBool", true);
+            transform.Translate(_model.forward * _speed * Time.fixedDeltaTime * 3f, Space.World);
+            passedTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+
+        _animator.SetBool("RunBool", false);
+        IsSpunt = false;
     }
 }
